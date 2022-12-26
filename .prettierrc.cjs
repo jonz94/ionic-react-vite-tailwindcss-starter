cc6c18230ef23bff3676189f074096fe013520d2
jonz94 <jody16888@gmail.com>
2022-12-26 09:56:41 +0800

    build: setup `prettier`

---
+++ .prettierrc.cjs
@@ -0,0 +1,9 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  bracketSameLine: false,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  printWidth: 120,
+}
